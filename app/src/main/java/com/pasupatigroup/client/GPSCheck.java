c783c6efd8bd6f6fbc08431034078ca136a55cf1
Nikhil Nayak <nikhilnayak98@gmail.com>
2016-11-18 23:49:26 +0530

    added alert siren if gps turned off

--- app/src/main/java/com/pasupatigroup/client/GPSCheck.java
+++ app/src/main/java/com/pasupatigroup/client/GPSCheck.java
@@ -4,6 +4,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.location.LocationManager;
+import android.media.AudioManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.widget.Toast;
 
 /**
@@ -12,16 +16,25 @@ import android.widget.Toast;
 
 public class GPSCheck extends BroadcastReceiver {
 
+    private AudioManager myAudioManager;
+    public Ringtone ringtone;
+
     @Override
     public void onReceive(Context context, Intent intent) {
-
         LocationManager locationManager = (LocationManager) context.getSystemService(context.LOCATION_SERVICE);
 
-        if(locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+        myAudioManager = (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);;
+        myAudioManager.setStreamVolume(4, myAudioManager.getStreamMaxVolume(4), 0);
+        Uri path = Uri.parse("android.resource://" + context.getPackageName() + "/raw/alert");
+        ringtone = RingtoneManager.getRingtone(context, path);
+        ringtone.setStreamType(4);
 
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            Toast.makeText(context, "Please Switch On The GPS", Toast.LENGTH_LONG).show();
         }
-        else {
-            Toast.makeText(context, "Please switch on the GPS", Toast.LENGTH_LONG).show();
+        while (!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            ringtone.play();
         }
+        ringtone.stop();
     }
 }
